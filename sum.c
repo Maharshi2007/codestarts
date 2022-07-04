22e4b9918aab6341d281a373b314c7fd8881ef6e
Maharshi2007 <103401242+Maharshi2007@users.noreply.github.com>
2022-07-04 20:26:26 +0530

    this is a sum program on c
    this is my frist program of my coding journey

---
+++ sum.c
@@ -0,0 +1,14 @@
+ #include<stdio.h>
+ #include<math.h>
+
+int main()  {
+    int a,b;
+    printf("enter a number :");
+    scanf("%d", &a);
+
+    printf("enter a number");
+    scanf("%d", &b);
+
+    printf("%d", a+b);
+    return 0;
+}
