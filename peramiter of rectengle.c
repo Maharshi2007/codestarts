3b1b4b95d03cd1e738e7a3dd12c5957dc47fa47b
Maharshi nimbark <103401242+Maharshi2007@users.noreply.github.com>
2022-07-09 22:35:56 +0530

    this is a program to find parameter of rectengle

---
+++ peramiter of rectengle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()  {
+    float lenth, brith;
+    printf("enter lenth");
+    scanf("%f", &lenth);
+
+    printf("enter brith");
+    scanf("%f", &brith);
+
+    printf("peramiter of rectengle is %f", lenth+lenth+brith+brith);
+    return 0;
+
+}
